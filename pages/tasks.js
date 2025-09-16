51708921e297e654051364709727328169c3c452
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-09-16 10:24:19 +0300

    feat: solve task 6kyu lottery ticket

--- pages/tasks.js
+++ pages/tasks.js
@@ -22,16 +22,43 @@
 // ссылка: https://www.codewars.com/kata/array-dot-diff/train/javascript
 // выход: If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 
-function arrayDiff(a, b) {
-  let arr = [];
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (b[j] === a[i] && !arr.includes(a[i])) {
-        arr.push(a[i]);
-      }
-    }
-  }
-  return a.filter((item) => !arr.includes(item));
+// function arrayDiff(a, b) {
+//   let arr = [];
+//   for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < b.length; j++) {
+//       if (b[j] === a[i] && !arr.includes(a[i])) {
+//         arr.push(a[i]);
+//       }
+//     }
+//   }
+//   return a.filter((item) => !arr.includes(item));
+// }
+
+// console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+
+// 3) название: Loterry ticket   6 kyu
+// ссылка: https://www.codewars.com/kata/lottery-ticket/train/javascript
+// выход:  assert.strictEqual(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2), 'Loser!');
+// assert.strictEqual(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1), 'Winner!');
+// assert.strictEqual(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3), 'Loser!');
+
+function bingo(ticket, win) {
+  let wins = 0;
+  ticket.forEach((item) => {
+    let char = item[0].split("");
+    let win = char.some((ticket) => ticket.charCodeAt() === item[1]);
+    if (win) wins++;
+  });
+  return wins >= win ? "Winner!" : "Loser!";
 }
 
-console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    2
+  )
+);
